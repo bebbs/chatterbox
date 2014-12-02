599f3b347f425c936b717cd38d441dfaf0e28245
Josh Bebbington <josh@focalwebsites.com>
2014-12-02 12:15:34 +0000

    Added further questions to the chatbot

--- chatbot.rb
+++ chatbot.rb
@@ -9,11 +9,17 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+              'Where are you?' => 'I am at Old Street Station',
+              'What is your name?' => 'My name is ChatBot',
+              'Who created you?' => 'Makers Academy created me'
+          }
 
 puts "Hello, what's your name?"
 name = gets.chomp
 puts "Hello #{name}"
 while(input = gets.chomp) do
   puts get_response(input)
+
 end
+
