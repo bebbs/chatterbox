8daeb28157618da99e7d158a84da15e526160a3a
Josh Bebbington <josh@focalwebsites.com>
2014-12-07 14:22:15 +0000

    Refactored file handling, cleaned up and commented code.

--- chatbot.rb
+++ chatbot.rb
@@ -1,5 +1,7 @@
+# Create an empty hash to contain questions and responses.
 RESPONSES = {}
 
+# get_response is a method to select the correct response for each input, including 4 regex capture groups.
 def get_response(input)
     key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
     /#{key}/ =~ input
@@ -7,6 +9,7 @@ def get_response(input)
     response.nil? ? 'sorry?' : response % { c1: $1, c2: $2, c3: $3, c4: $4}
 end
 
+# ask_user and bot_response handle getting user input and displaying the response. 
 def ask_user
 	print "User: "
 	gets.chomp
@@ -16,6 +19,7 @@ def bot_response(response)
 	puts "Chatbot: #{response}"
 end
 
+# add_response allows you to add custom questions and set a response from the chatbot.
 def add_response
 	puts "What question would you like to add?: "
 	q = gets.chomp
@@ -23,15 +27,19 @@ def add_response
 	a = gets.chomp
 	RESPONSES[q] = a
     puts "Response added!"
+    puts ""
 end
 
-def get_difference?(hash1, hash2)
-    difference = {}
+# Single responsibility method to open a file in either read or write mode.
+def file_handler(filename, mode)
+    @file = File.open(filename, mode)
 end
 
+# Loads questions.csv in read mode, iterates through and splits each line into 
+# key => value, then pushes the pair into the RESPONSES hash.
 def load_questions
-    file = File.open("questions.csv", "r")
-    file.readlines.each do |line|
+    file_handler("questions.csv", "r")
+    @file.readlines.each do |line|
         key, value = line.chomp.split(" | ")
         RESPONSES[key] = value
     end
@@ -39,26 +47,35 @@ def load_questions
     puts ""
 end
 
+# Opens questions.csv in write mode, then appends each key => value pair 
+# to the file, split with ' | '. 
 def save_questions
-    file = File.open("questions.csv", "w")
+    file_handler("questions.csv", "w")
     RESPONSES.each do |key, value|
-        file.puts key + " | " + value
+        @file.puts key + " | " + value
     end
-    file.close
+    @file.close
     puts "Questions have been saved to questions.csv!"
 end
 
+# Calls save_questions and exits the program.
 def save_and_quit
     puts "Thanks for chatting with chatbot!"
     save_questions
     exit
 end
 
-def program_loop
+# Begins the program by learning the users name.
+def say_hello
     puts "Hello, what's your name?"
     print "User: "
     name = gets.chomp
     bot_response("Hello #{name}")
+end
+
+# A loop to continually ask for user input, and direct it's response to 
+# either quit the program, add a new response, or fetch the bot response.
+def program_loop
     while(input = ask_user) do
     	if input == "quit"
             save_and_quit
@@ -70,8 +87,10 @@ def program_loop
     end
 end
 
+# Loads the initial set of questions, says hello, then starts the program_loop.
 def start_program
     load_questions
+    say_hello
     program_loop
 end
 
